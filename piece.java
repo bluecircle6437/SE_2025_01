67f9e7217b905325851bee17d3daeff07ca0fcb9
bluecircle6437 <bluecircle6437@gmail.com>
2025-04-27 18:48:46 +0900

    클래스 명명 규칙 위반으로 인해 삭제 후 재생성

--- piece.java
+++
@@ -1,4 +0,0 @@
-public class piece {
-    int pieceNum; //식별 번호
-    int location; //위치
-}
--- player.java
+++
@@ -1,5 +0,0 @@
-public class player {
-    String name; //플레이어명
-    int order; //순서
-    //piece(말)은 설정에 따라 동적으로 생성
-}
