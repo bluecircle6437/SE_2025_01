47dceeb855b77fbd86157c4dd3406897b154ab0b
정민직 <thunder101880@gmail.com>
2025-05-03 19:19:45 +0900

    Player.java 업데이트
    클래스 선언 및 기본설정

--- Player.java
+++ Player.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+//Player는 name, Piece는 Id로 구별
 public class Player {
-    String name; //플레이어명
-    int order; //순서
-    //piece(말)은 설정에 따라 동적으로 생성
+    private int name;
+    private int order; // 턴 순서
+    private List<Piece> pieces;
+    private int arrivedCount;
+
+    public Player(int name) {
+        this.name = name;
+        this.order = order;
+        this.pieces = new ArrayList<>();
+        this.arrivedCount = 0;
+    }
+
+    public void addPiece() {
+        int pieceId = pieces.size();
+        pieces.add(new Piece(pieceId));
+    }
+
+    // Getter & Setter
+    public int getname() { return name; }
+    public int getOrder() { return order; }
+    public void setOrder(int order) { this.order = order; }
+    public List<Piece> getPieces() { return pieces; }
+    public int getArrivedCount() { return arrivedCount; }
+    public void setArrivedCount(int count) { this.arrivedCount = count; }
+
+    public boolean allPiecesArrived() {
+        return arrivedCount == pieces.size();
+    }
 }
